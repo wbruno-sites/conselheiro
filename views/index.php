0fcbafde8ed83c444c2d2f6e149159dfce8b5010
William Bruno <rocha_bruno@hotmail.com>
2015-11-20 16:44:03 -0200

    Improve index

--- views/index.php
+++ views/index.php
@@ -1,6 +1,8 @@
 <div class="container-fluid">
     <div class="row">
 
-    <h1><font size="26"> SISTEMA DE APURAÇÃO! <br> ATENÇÃO: Todas as informações inseridas nos formulários devem ser conferidas antes da gravação!</font></h1>
-    </div>
-</div>
+      <h1><font size="26"> SISTEMA DE APURAÇÃO!</h1>
+      <h2>ATENÇÃO: Todas as informações inseridas nos formulários devem ser conferidas antes da gravação!</h2>
+
+    </div><!-- .row -->
+</div><!-- .container-fluid -->
