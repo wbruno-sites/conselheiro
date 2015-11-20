a6058628d1e28eaf6ec161cd8ccf85d36d03d921
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-11-20 17:34:19 -0200

    Remove error message on redirect logout

--- app/logout-action.php
+++ app/logout-action.php
@@ -3,4 +3,4 @@ require '../config/bootstrap.php';
 
 session_unset();
 session_destroy();
-header("Location: http://localhost:8080/login.php?error=$message");
+header("Location: http://localhost:8080/login.php");
