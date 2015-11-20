864ea9d5a58326382290ba15f6e348e97d7f20e5
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-11-20 14:19:20 -0200

    Add cadastrar usuario form

---
+++ views/cadastrar-usuario.php
@@ -0,0 +1,23 @@
+<br><br>
+<div class="col-md-12">
+  <form action="" method="POST">
+    <fieldset>
+      <legend>Cadastrar User</legend>
+      <div class="form-group">
+        <label for="login">Login</label>
+        <input type="text" name="login" id="login" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="password">Senha</label>
+        <input type="password" name="password" id="password" class="form-control">
+      </div>
+      <div class="form-group">
+        <input type="checkbox" name="admin" id="admin" value="admin">
+        <label for="admin">Admin?</label>
+      </div>
+
+      <button class="btn btn-primary">Cadastrar</button>
+    </fieldset>
+  </form>
+
+</div>
