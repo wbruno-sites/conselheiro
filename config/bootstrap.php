0a0f9709fc1474ed610ded137a52426c9f3ae9f9
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-11-20 13:00:21 -0200

    Change consts to bootstrap file

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+ini_set('include_path', dirname(__FILE__));
+
+# CONSTS
+define('CONFIG_FILE', 'config.ini');
+
+
 session_start();
+
+# INCLUDE
 require_once('database.php');
 require_once('functions.php');
--- config/database.php
+++ config/database.php
@@ -1,8 +1,5 @@
 <?php
 
-ini_set('include_path', dirname(__FILE__));
-define('CONFIG_FILE', 'config.ini');
-
 $configs = parse_ini_file(CONFIG_FILE);
 
 $mysqli = new mysqli($configs['mysql']['host'], $configs['mysql']['user'], $configs['mysql']['password'], $configs['mysql']['database']);
