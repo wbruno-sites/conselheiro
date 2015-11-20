ac6361e49d1cf85481a669d3578025def524adc6
William Bruno <rocha_bruno@hotmail.com>
2015-11-20 11:15:46 -0200

    Include config file

--- config/database.php
+++ config/database.php
@@ -1,1 +1,16 @@
 <?php
+
+define('CONFIG_FILE', './config.ini');
+
+$configs = parse_ini_file(CONFIG_FILE);
+
+var_dump($configs);
+
+
+$mysqli = new mysqli($configs['mysql']['host'], $configs['mysql']['user'], $configs['mysql']['password'], $configs['mysql']['database']);
+
+$query = $mysqli->query('SELECT * FROM candidates');
+
+while($data = $query->fetch_object()) {
+  echo $data->id, '-' ,$data->name, '-' ,$data->region_id;
+}
