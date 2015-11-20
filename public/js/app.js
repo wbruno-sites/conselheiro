cbd2ccae7cbf1c1afe229906a266b91a84ecbcec
William Bruno <rocha_bruno@hotmail.com>
2015-11-20 13:24:52 -0200

    Include rooms amount on seed

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,31 @@ $("#menu-toggle").click(function(e) {
 $('input[name="votes_amount[]"]').on('keyup', function() {
   mascara(this, mnum);
 });
+
+
+var $school_id = $('select[name="school_id"]'),
+    $room_id = $('select[name="room_id"');
+
+$school_id.on('change', function() {
+  var $this = $(this),
+      rooms_amount = $(this).find('option:selected').data('rooms-amount'),
+      options = [],
+      i = 1;
+
+
+      console.log(rooms_amount);
+
+  while(rooms_amount--) {
+    options.push('<option values="' + i + '">' + i + '</option>');
+    i++;
+  }
+
+  $room_id.html(options.join(','));
+
+  history.pushState({}, '', '?school_id=' + $this.val() + '&room_id=1');
+});
+
+$room_id.on('change', function() {
+  var $this = $(this);
+  history.pushState({}, '', '?school_id=' + $school_id.val() + '&room_id=' + $this.val());
+});
