1315c32b8c3182e2715322e628a8ca4f8240e373
William Bruno <rocha_bruno@hotmail.com>
2015-11-20 19:51:01 -0200

    set charset

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 $GLOBALS['mysqli'] = new mysqli($GLOBALS['config']['mysql']['host'], $GLOBALS['config']['mysql']['user'], $GLOBALS['config']['mysql']['password'], $GLOBALS['config']['mysql']['database']);
 
+$GLOBALS['mysqli']->set_charset("utf8");
+
 if($GLOBALS['mysqli']->connect_errno) {
   $GLOBALS['error'] = $GLOBALS['mysqli']->connect_errno . ' - ' . $GLOBALS['mysqli']->connect_error;
 }
